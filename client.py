9d2e856b5d120254ec23b3ad25ddaddb96e04c13
Philippe Vienne <philippegeek@gmail.com>
2018-01-04 03:09:18 +0100

    Checkout files from website

--- client.py
+++ client.py
@@ -1,5 +1,10 @@
+import hashlib
 import re
 from datetime import datetime, timedelta
+
+import os
+import requests
+from bs4 import BeautifulSoup
 
 import xlrd
 
@@ -7,7 +12,6 @@ regex = r"((([0-9]{3,4})(a|sa|sd|DL)?)[ \/])?([0-9]{3,4}) x ((([0-9]{3,4})(a|sa|
 
 
 class Mission:
-
     num_mission = None
     prise_mission = None
     lieu_mission = None
@@ -60,6 +64,65 @@ class Mission:
         return ' '.join(self.to_row_value())
 
 
+class Crawler:
+
+    folder = os.path.join(os.getcwd(), 'data')
+
+    def __init__(self, login, password, url="http://etutcl.fr"):
+        try:
+            os.mkdir(self.folder)
+        except FileExistsError:
+            pass
+        self.password = password
+        self.login = login
+        self.url = url
+        self.session = requests.Session()
+        self.cookies = requests.cookies.RequestsCookieJar()
+
+    def login_on_site(self):
+        r = self.session.post(
+            self.url + '/login/request',
+            data={'username': self.login, 'password': self.password}, cookies=self.cookies)
+        content = str(r.content, 'utf-8')
+        if r.status_code > 300 or 'Mot de passe incorrect' in content:
+            raise Exception('Wrong login!')
+        if r.history:
+            for historicResponse in r.history:
+                self.cookies.update(historicResponse.cookies)
+        self.cookies.update(r.cookies)
+
+    def load_dispos(self):
+        r = self.session.get(self.url + '/dispos', cookies=self.cookies)
+        content = str(r.content, 'utf-8')
+        soup = BeautifulSoup(content, 'html.parser')
+        tables = soup.find_all('table', "data")
+        works = []
+        for table in tables:
+            first_line = table.find('tbody').findAll('tr')[0].findAll('td')
+            day = first_line[0]
+            day = str(day.find(text=re.compile("[0-9]{2}/[0-9]{2}/[0-9]{2}"))).strip()
+            data = first_line[-1]
+            a_tag = data.find('a')
+            if a_tag is None:
+                continue
+            group = int(data.find(text=re.compile("Groupe attribué")).nextSibling.string)
+            link = a_tag.attrs['href']
+            day = datetime.strptime(day, "%d/%m/%y")
+            self.download(link)
+            works += list_horaires(import_excel(self.path_for(link)), group, day)
+        return works
+
+    def download(self, file):
+        path = self.path_for(file)
+        r = self.session.get(self.url + file, stream=True, cookies=self.cookies)
+        if r.status_code == 200:
+            with open(path, 'wb') as f:
+                for chunk in r:
+                    f.write(chunk)
+
+    def path_for(self, file):
+        return os.path.join(self.folder, hashlib.md5(file.encode('utf-8')).hexdigest())
+
 
 def import_excel(excel):
     excelHoraire = xlrd.open_workbook(excel)
@@ -85,15 +148,17 @@ def list_horaires(sheet, groupe, jour):
             row = row_value
             break
     if row is None:
-        raise Exception('Groupe introuvable!')
+        raise Exception('Groupe '+ groupe + ' introuvable!')
     for x in range(row.count('')):
         row.remove('')
-    works = [Mission(row[i], row[i+1], jour) for i in range(0, len(row), 2)]
-    [print(work) for work in works]
+    works = [Mission(row[i], row[i + 1], jour) for i in range(0, len(row), 2)]
+    return works
 
 
 if __name__ == "__main__":
-    path = 'data-test/test 1.xls'
-    gpe = 40
-    excelHoraires = import_excel(path)
-    list_horaires(excelHoraires, gpe, datetime(2018, 1, 3))
+    c = Crawler(os.getenv('LOGIN'), os.getenv('PASSWORD'))
+    c.login_on_site()
+    works = c.load_dispos()
+    for work in works:
+        print(work)
+    # list_horaires(excelHoraires, gpe, datetime(2018, 1, 3))
